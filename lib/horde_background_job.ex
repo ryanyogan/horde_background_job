9054ff8735a9fe9b2285837e5b94ea7a0dc8475a
ryan <ryanyogan@gmail.com>
2021-06-30 13:14:14 -0500

    initial

---
+++ lib/horde_background_job.ex
@@ -0,0 +1,18 @@
+defmodule HordeBackgroundJob do
+  @moduledoc """
+  Documentation for `HordeBackgroundJob`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HordeBackgroundJob.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
